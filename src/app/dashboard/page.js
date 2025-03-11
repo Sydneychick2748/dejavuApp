f5dd47d8ff19f880d9272d9c11f42e24180102fa
Tberry <Tberry@anniecannons.com>
2025-03-11 18:09:01 -0500

    adding uploaded updates

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -22,20 +22,21 @@ export default function Dashboard() {
       >
         {/* Left Section (Upload Files) */}
         <Box 
-           flex="0 0 35%"
+          flex="0 0 40%"
           bg="white"
           p={4}
           borderRadius="lg"
           shadow="md"
           // w={{ base: "100%", lg: "30%" }} // Full width on mobile/tablet, 50% on desktop
         >
+          
           <UploadFiles />
         </Box>
 
         {/* Right Section (Search & Parameters) */}
         <Flex
           direction="column"
-          flex="0 0 65%"
+          flex="0 0 60%"
           gap={4}
           // w={{ base: "100%", lg: "70%" }} // Full width on mobile/tablet, 50% on desktop
         >
