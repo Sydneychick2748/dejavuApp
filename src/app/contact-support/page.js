1cc93df296bad70b53bc1640bbb8ebc358aae36d
Kalevitutar <tanyamthomas@yahoo.com>
2025-04-07 15:05:23 -0400

    changed header and menu to match new Figma designs

---
+++ src/app/contact-support/page.js
@@ -0,0 +1,12 @@
+"use client"
+
+import "./contact-support.css";
+
+export default function ContactSupport() {
+
+    return (
+        <>
+            <h1>HI - We are here and eager to help!</h1>
+        </>
+    )
+}
