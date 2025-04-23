a474637e55c2cb5768effa17e9935def0c85759e
ana <ana@anniecannons.com>
2025-04-23 10:20:42 -0700

    added file structure for the dropdown

--- src/app/windowSizeCalc.js
+++
@@ -1,9 +0,0 @@
-export default function windowSizeCalc() {
-    const headerHeight = document.querySelector('header').offsetHeight;
-    const footerHeight = document.querySelector('footer').offsetHeight;
-    const viewportHeight = window.innerHeight;
-
-    const windowSizeCalc = viewportHeight - (headerHeight + footerHeight);
-
-    return windowSizeCalc;
-}
