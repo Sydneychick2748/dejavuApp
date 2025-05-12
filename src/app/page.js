53a16745eefdba20dfa8b52c31ad9c6cafdb8947
Tberry <Tberry@anniecannons.com>
2025-05-12 00:49:45 -0500

    updating home by changing paragraph n link under title

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-"use client"
+"use client";
 
 import "./home-page.css";
 import { useRouter } from "next/navigation"; // Import useRouter
 import LoginBox from "../app/accounts/login/page";
-
 
 export default function Home() {
   const router = useRouter(); // Initialize router
@@ -19,11 +18,11 @@ export default function Home() {
             <strong>right at your fingertips.</strong>
           </h1>
           <p className="home-description">
-            Photon is a personal image-search application that utilizes a
-            specialized AI pattern-matching technology, unlike anything else on
-            the market.
+          DejaVuAI’s personal image-search application utilizes a specialized AI pattern-matching technology, unlike anything else on the market.
           </p>
-          <a href="#" className="learn-link">Learn more ›</a>
+          <a href="#" className="learn-link">
+            About The Application ›
+          </a>
 
           <div className="powered-box">
             {/* <span className="poweredBy">Powered by</span> */}
@@ -35,7 +34,6 @@ export default function Home() {
           </div>
         </div>
 
-
         <div className="right-section">
           <LoginBox />
         </div>
@@ -43,4 +41,3 @@ export default function Home() {
     </div>
   );
 }
-
